d6f8b75c5b49d87aacc314a11ae147dfb2f4cd93
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-03 20:03:30 -0400

    metadata change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Haroon Helps`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Haroon Choudery`,
+      summary: `Helping futureproof you in a changing world.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Helping futureproof you in a changing world.`,
+    siteUrl: `https://haroonhelps.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `haroonchoudery`,
     },
   },
   plugins: [
