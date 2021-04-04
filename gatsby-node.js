ad5343629ff110edd0762b4a3743e11c6f9f64a5
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-04 00:18:40 -0400

    kinda working now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,25 @@
 const path = require(`path`)
-const { postsPerPage } = require(`./src/utils/siteConfig`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
-const { paginate } = require(`gatsby-awesome-pagination`)
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
+
+  // Define a template for blog post
+  const postTemplate = path.resolve(`./src/templates/post.js`)
 
   // Get all Ghost blog posts sorted by date
   const result = await graphql(
     `
       {
-        allGhostPost(sort: { order: ASC, fields: published_at }) {
-            edges {
-                node {
-                    slug
-                }
+        allGhostPost(sort: {fields: published_at, order: ASC}, limit: 1000) {
+          edges {
+            node {
+              slug
+              id
             }
+          }
         }
-        allGhostAuthor(sort: { order: ASC, fields: name }) {
-            edges {
-                node {
-                    slug
-                    url
-                    postCount
-                }
-            }
-        }
-        allGhostPage(sort: { order: ASC, fields: published_at }) {
-            edges {
-                node {
-                    slug
-                    url
-                }
-            }
-        }
-    }
+      }
     `
   )
 
@@ -50,127 +35,35 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
 
-  // const tags = result.data.allGhostTag.edges
-  const authors = result.data.allGhostAuthor.edges
-  const pages = result.data.allGhostPage.edges
-  const posts = result.data.allGhostPost.edges
-
-  // Define a template for blog post
-  // const indexTemplate = path.resolve(`./src/templates/index.js`)
-  // const tagsTemplate = path.resolve(`./src/templates/tag.js`)
-  const authorTemplate = path.resolve(`./src/templates/author.js`)
-  const pageTemplate = path.resolve(`./src/templates/page.js`)
-  const postTemplate = path.resolve(`./src/templates/post.js`)
-
-  //   // Create tag pages
-  // tags.forEach(({ node }) => {
-  //   const totalPosts = node.postCount !== null ? node.postCount : 0
-
-  //   // This part here defines, that our tag pages will use
-  //   // a `/tag/:slug/` permalink.
-  //   const url = `/tag/${node.slug}`
-
-  //   const items = Array.from({length: totalPosts})
-
-  //   // Create pagination
-  //   paginate({
-  //       createPage,
-  //       items: items,
-  //       itemsPerPage: postsPerPage,
-  //       component: tagsTemplate,
-  //       pathPrefix: ({ pageNumber }) => (pageNumber === 0) ? url : `${url}/page`,
-  //       context: {
-  //           slug: node.slug
-  //       }
-  //   })
-  // })
-
-  // // Create author pages
-  // authors.forEach(({ node }) => {
-  //     const totalPosts = node.postCount !== null ? node.postCount : 0
-
-  //     // This part here defines, that our author pages will use
-  //     // a `/author/:slug/` permalink.
-  //     const url = `/author/${node.slug}`
-
-  //     const items = Array.from({length: totalPosts})
-
-  //     // Create pagination
-  //     paginate({
-  //         createPage,
-  //         items: items,
-  //         itemsPerPage: postsPerPage,
-  //         component: authorTemplate,
-  //         pathPrefix: ({ pageNumber }) => (pageNumber === 0) ? url : `${url}/page`,
-  //         context: {
-  //             slug: node.slug
-  //         }
-  //     })
-  // })
-
-  // Create pages
-  pages.forEach(({ node }) => {
-      // This part here defines, that our pages will use
-      // a `/:slug/` permalink.
-      node.url = `/${node.slug}/`
-
-      createPage({
-          path: node.url,
-          component: pageTemplate,
-          context: {
-              // Data passed to context is available
-              // in page queries as GraphQL variables.
-              slug: node.slug,
-          },
-      })
+  const items = result.data.allGhostPost.edges
+  items.forEach(({ node }) => {
+    node.url = `/${node.slug}/`
+    actions.createPage({
+      path: node.url,
+      component: postTemplate,
+      context: {
+        slug: node.slug,
+        // id: node.id,
+        // previousPostId,
+        // nextPostId
+      },
+    })
   })
-
-  // Create post pages
-  posts.forEach(({ node }) => {
-      // This part here defines, that our posts will use
-      // a `/:slug/` permalink.
-      node.url = `/${node.slug}/`
-
-      createPage({
-          path: node.url,
-          component: postTemplate,
-          context: {
-              // Data passed to context is available
-              // in page queries as GraphQL variables.
-              slug: node.slug,
-          },
-      })
-  })
-
-  // // Create pagination
-  // paginate({
-  //     createPage,
-  //     items: posts,
-  //     itemsPerPage: postsPerPage,
-  //     component: indexTemplate,
-  //     pathPrefix: ({ pageNumber }) => {
-  //         if (pageNumber === 0) {
-  //             return `/`
-  //         } else {
-  //             return `/page`
-  //         }
-  //     },
-  // })
 }
 
-// exports.onCreateNode = ({ node, actions, getNode }) => {
-//   const { createNodeField } = actions
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
 
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const value = createFilePath({ node, getNode })
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
 
-//     createNodeField({
-//       name: `slug`,
-//       node,
-//       value,
-//     })
-//   }
-// }
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
 
 // exports.createSchemaCustomization = ({ actions }) => {
 //   const { createTypes } = actions
