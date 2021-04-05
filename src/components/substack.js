70489f915f389c55b7257dae4262a6fc5acd7aa0
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-04 20:12:39 -0400

    substack 100% width

--- src/components/substack.js
+++ src/components/substack.js
@@ -6,7 +6,7 @@ const SubstackForm = () => {
       <div>
         <h1 className="alt-header">Subscribe</h1>
         <p>I send an email newsletter each week with the links I thought were interesting, plus any new posts or projects from me. There are 1,500+ subscribers. You should sign up too.</p>
-        <div css={`text-align: center;`}><iframe src="https://haroon.substack.com/embed" width="480" height="120" frameborder="0" scrolling="no"></iframe></div>
+        <div css={`text-align: center;`}><iframe src="https://haroon.substack.com/embed" width="100%" height="120" frameborder="0" scrolling="no"></iframe></div>
       </div>
     )
   }
