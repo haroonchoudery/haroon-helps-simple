e2a62da22237dddf444937537d20f3e8312e9c88
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-05 14:06:35 -0400

    nonprofit link external

--- src/components/NavbarLinks.js
+++ src/components/NavbarLinks.js
@@ -39,12 +39,51 @@ const NavItem = styled(Link)`
     z-index: 6;
   }
 `
+
+const NavItemExternal = styled.a`
+  text-decoration: none;
+  color: #111;
+  display: inline-block;
+  white-space: nowrap;
+  margin: 0 1vw;
+  transition: all 200ms ease-in;
+  position: relative;
+  font-family: var(--fontFamily-alt);
+  letter-spacing: 0.2em;
+
+  :after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 0%;
+    content: ".";
+    color: transparent;
+    background: goldenrod;
+    height: 1px;
+    transition: all 0.4s ease-in;
+  }
+
+  :hover {
+    color: goldenrod;
+    ::after {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 20px 0;
+    font-size: 1.5rem;
+    z-index: 6;
+  }
+`
+
 const NavbarLinks = () => {
   return (
     <>
       <NavItem to="/blog">Blog</NavItem>
       <NavItem to="/">About</NavItem>
-      <NavItem to="https://aiforanyone.org">Nonprofit</NavItem>
+      <NavItemExternal href="https://aiforanyone.org" target="_blank">Nonprofit</NavItemExternal>
     </>
   )
 }
