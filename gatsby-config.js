bb95041610d13fc6870689d2f7a1b2226994ef1e
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-05 20:12:38 -0400

    trying to fix embed issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,6 +113,13 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        mergeLinkHeaders: false,
+        mergeCachingHeaders: false
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
