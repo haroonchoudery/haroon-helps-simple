7ea8c9a4ce6dbe9fa7e1afee7f3d9935de57bb3a
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-05 20:24:48 -0400

    commented out service workers code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,4 +11,4 @@ import "./src/custom.css"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 
-export const onServiceWorkerUpdateReady = () => window.location.reload();
+// export const onServiceWorkerUpdateReady = () => window.location.reload();
