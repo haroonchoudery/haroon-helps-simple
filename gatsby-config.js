10a46ab61d25f970ad4766120f4f860d8d3903f3
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-05 20:46:44 -0400

    testing something

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,8 +120,6 @@ module.exports = {
         mergeCachingHeaders: false
       },
     },
-    `gatsby-plugin-instagram`,
-    `gatsby-plugin-twitter`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
