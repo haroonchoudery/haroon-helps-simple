7caee7e86079acb209965a32dfc446e67e6ebeb0
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-11 22:36:39 -0400

    nothing [skip ci]

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,6 +5,21 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import SubstackForm from "../components/substack"
 
+const ExternalLink = props => {
+	if (props.href.includes('haroonhelps.com') || props.href[0] === '/') {
+		return <a href={props.href}>{props.children}</a>
+	}
+	return (
+		<a href={props.href} target="_blank" rel="noopener noreferrer">
+			{props.children}
+		</a>
+	)
+}
+
+const components = {
+	a: ExternalLink,
+}
+
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.ghostPost
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -13,6 +28,16 @@ const BlogPostTemplate = ({ data, location }) => {
   const publish_date = post.published_at || "Undated"
   const reading_time = post.reading_time + 1 || "1"
   const category = post.tags[0]?.name || "Uncategorized"
+
+  const GhostHTML = () =>  {
+    const post = data.ghostPost
+    return(
+      <section
+      dangerouslySetInnerHTML={{ __html: post.html}}
+      itemProp="articleBody">
+      </section>
+    )
+  }
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -29,10 +54,7 @@ const BlogPostTemplate = ({ data, location }) => {
           <h1 itemProp="headline">{post.title}</h1>
           <p>{publish_date} • {reading_time} min • {category}</p>
         </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
+        <GhostHTML components={components} />
         <hr className="spacing-top" />
         <footer>
           <SubstackForm />
