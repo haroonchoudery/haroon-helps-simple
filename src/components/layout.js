372bdcd9584be89ba4bcc88df4ad3b90a79cf2bb
Haroon Choudery <haroon.choudery@gmail.com>
2021-04-11 22:36:28 -0400

    small things [skip ci]

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,7 @@
 import * as React from "react"
+import { Helmet } from "react-helmet";
 import Navbar from "./Navbar"
 import Footer from "./footer"
-
-const ExternalLink = props => {
-	if (props.href.includes('haroonhelps.com') || props.href[0] === '/') {
-		return <a href={props.href}>{props.children}</a>
-	}
-	return (
-		<a href={props.href} target="_blank" rel="noopener noreferrer">
-			{props.children}
-		</a>
-	)
-}
-
-const components = {
-	a: ExternalLink,
-}
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -29,7 +15,7 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
-      <main components={components}>{children}</main>
+      <main>{children}</main>
       <Footer />
     </div>
   )
